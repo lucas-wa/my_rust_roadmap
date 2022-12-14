1bf2c072c17de435d70e74b35730b1e0ab55a6c2
lucas-wa <lucaswanderley600@gmail.com>
2022-12-13 23:57:29 -0300

    Trait Debug

--- exercices/src/main.rs
+++ exercices/src/main.rs
@@ -1,7 +1,22 @@
 
+struct Rectangle {
+    lenght: u32,
+    width: u32
+}
 
 fn main() {
-     
+     let rect1 = Rectangle { 
+        lenght: 50, 
+        width: 30
+    };
+
+    println!("Area: {}", rectangle_area(&rect1));
+
+
+}
+
+fn rectangle_area(rectangle: &Rectangle) -> u32 {
+    rectangle.lenght * rectangle.width
 }
 
 
