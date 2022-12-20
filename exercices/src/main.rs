4c1c1ec194f59cd7bebccb94d8b85d18f209a217
lucas-wa <lucaswanderley600@gmail.com>
2022-12-19 23:17:11 -0300

    Retangle file added

--- exercices/src/main.rs
+++ exercices/src/main.rs
@@ -1,37 +1,3 @@
-#[derive(Debug)]
-
-struct Rectangle {
-    lenght: u32,
-    width: u32
+fn main(){
+    
 }
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.lenght * self.width
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.lenght > other.lenght
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle { lenght: size, width: size }
-    }
-}
-
-fn main() {
-    let square1 = Rectangle::square(5);
-
-    println!("{:#?}", square1);
-
-
-}
-
-// fn rectangle_area(rectangle: &Rectangle) -> u32 {
-//     rectangle.lenght * rectangle.width
-// }
-
-
-
- 
-
