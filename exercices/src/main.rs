228a6ca5073920eeed784931c87d351d3e5980a1
lucas-wa <lucaswanderley600@gmail.com>
2022-12-22 23:52:06 -0300

    Use enums

--- exercices/src/main.rs
+++ exercices/src/main.rs
@@ -1,3 +1,14 @@
+
+
 fn main(){
+
+    // Using a enum to not use a struct
+    enum ipAdress {
+        V4(u8, u8, u8, u8),
+        V8(String),
+    }
+
+    let local = ipAdress::V4(127, 0, 0, 1); 
     
+    let loopback = ipAdress::V6(String::from("::01"))
 }
