50ee2c4f7524d29f069655a9f79ea7d5260673e8
lucas-wa <lucaswanderley600@gmail.com>
2022-12-24 18:28:56 -0300

    Using Match

--- exercices/src/main.rs
+++ exercices/src/main.rs
@@ -2,13 +2,27 @@
 
 fn main(){
 
-    // Using a enum to not use a struct
-    enum ipAdress {
-        V4(u8, u8, u8, u8),
-        V8(String),
+    enum State {
+        Alabama,
+        Alaska,
     }
 
-    let local = ipAdress::V4(127, 0, 0, 1); 
-    
-    let loopback = ipAdress::V6(String::from("::01"))
+    enum Coin {
+        Penny,
+        Nickel, 
+        Dime,
+        Quarter(State),
+    }
+
+    fn coin_value(coin : Coin) -> u32{
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("Quarter do estado {:?}", state);
+                25   
+            },
+        }
+    }
 }
